288b5bdbf8047c601993a1cf4acfe46d7e7d5316
Mozahid Izhar <mozahidizhar54@gmail.com>
2026-02-24 13:20:02 +0530

    casesensetive issue

--- Gulam-sir-Acadamy/src/Pages/ImportImg/GalleryImg.js
+++ Gulam-sir-Acadamy/src/Pages/ImportImg/GalleryImg.js
@@ -1,14 +1,3 @@
-// import img1 from "../../assets/GalleryImage/IMG20251218125903.jpg"
-// import img2 from "../../assets/GalleryImage/IMG_20251216_192910.jpg"
-// import img3 from "../../assets/GalleryImage/IMG_20251221_192623.jpg"
-// import img4 from "../../assets/GalleryImage/IMG_20251221_192648.jpg"
-// import img5 from "../../assets/GalleryImage/IMG_20251221_192648.jpg"
-// import img6 from "../../assets/GalleryImage/IMG_20251221_192727.jpg"
-// import img7 from "../../assets/GalleryImage/IMG_20251221_192757.jpg"
-// import img8 from "../../assets/GalleryImage/IMG_20251221_192921.jpg"
-// import img9 from "../../assets/GalleryImage/WhatsApp Image 2026-01-24 at 9.20.25 PM"
-// import img10 from "../../assets/GalleryImage/WhatsApp Image 2026-01-24 at 9.21.42 PM"
-
 
 import img1 from "../../assets/GalleryImage/IMG20251218125903.jpg";
 import img2 from "../../assets/GalleryImage/IMG_20251216_192910.jpg";
