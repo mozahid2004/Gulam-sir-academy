468330a574cd3a0de93e272d241de4c767a8167f
Mozahid Izhar <mozahidizhar54@gmail.com>
2026-02-24 13:33:04 +0530

    casesensetive imge name issue

--- Gulam-sir-Acadamy/src/Pages/ImportImg/GalleryImg.js
+++ Gulam-sir-Acadamy/src/Pages/ImportImg/GalleryImg.js
@@ -1,21 +1,19 @@
-
-import img1 from "../../assets/GalleryImage/IMG20251218125903.jpg";
-import img2 from "../../assets/GalleryImage/IMG_20251216_192910.jpg";
-import img3 from "../../assets/GalleryImage/IMG_20251221_192623.jpg";
-import img4 from "../../assets/GalleryImage/IMG_20251221_192648.jpg";
-import img5 from "../../assets/GalleryImage/whatsApp1.jpg"
-import img6 from "../../assets/GalleryImage/IMG_20251221_192727.jpg";
-import img7 from "../../assets/GalleryImage/IMG_20251221_192757.jpg";
-import img8 from "../../assets/GalleryImage/IMG_20251221_192921.jpg";
-import img9 from "../../assets/GalleryImage/WhatsApp2.jpg";
-import img10 from "../../assets/GalleryImage/WhatsApp3.jpg";
-import img11 from "../../assets/GalleryImage/WhatsApp4.jpg";
-import img12 from "../../assets/GalleryImage/WhatsApp5.jpg";
-import img13 from "../../assets/GalleryImage/WhatsApp6.jpg";
-import img14 from "../../assets/GalleryImage/WhatsApp7.jpg";
-import img15 from "../../assets/GalleryImage/WhatsApp8.jpg";
-import img16 from "../../assets/GalleryImage/WhatsApp9.jpg";
-
+import img1 from "../../assets/GalleryImage/whatsapp1.jpg";
+import img2 from "../../assets/GalleryImage/whatsapp2.jpg";
+import img3 from "../../assets/GalleryImage/whatsapp3.jpg";
+import img4 from "../../assets/GalleryImage/whatsapp4.jpg";
+import img5 from "../../assets/GalleryImage/whatsapp5.jpg";
+import img6 from "../../assets/GalleryImage/whatsapp6.jpg";
+import img7 from "../../assets/GalleryImage/whatsapp7.jpg";
+import img8 from "../../assets/GalleryImage/whatsapp8.jpg";
+import img9 from "../../assets/GalleryImage/whatsapp9.jpg";
+import img10 from "../../assets/GalleryImage/whatsapp10.jpg";
+import img11 from "../../assets/GalleryImage/whatsapp11.jpg";
+import img12 from "../../assets/GalleryImage/whatsapp12.jpg";
+import img13 from "../../assets/GalleryImage/whatsapp13.jpg";
+import img14 from "../../assets/GalleryImage/whatsapp14.jpg";
+import img15 from "../../assets/GalleryImage/whatsapp15.jpg";
+import img16 from "../../assets/GalleryImage/whatsapp16.jpg";
 
 export const galleryImages = [
   [img1, img2, img3, img16],
